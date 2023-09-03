f761e8b63d2650273291e10e329524b8c6a445cb
Chitra Mukhiya <chitranjan.it@gmail.com>
2023-09-03 07:43:03 +0530

    api url is changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/api/users',(req ,res) => {
+app.get('/api',(req ,res) => {
 
     const users = [
         {
